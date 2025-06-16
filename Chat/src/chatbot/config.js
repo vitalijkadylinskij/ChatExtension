7abc13f3494298e45c41bc5507424be765228111
vitalijkadylinskij <vitalijkadylinskij@gmail.com>
2025-06-16 13:09:39 +0300

    fix custom style

--- Chat/src/chatbot/config.js
+++ Chat/src/chatbot/config.js
@@ -3,14 +3,7 @@ import { createChatBotMessage } from "react-chatbot-kit"
 const config = {
     initialMessages: [createChatBotMessage(`Привет! Я ваш AI-помощник. Задайте мне любой вопрос!`)],
     botName: "AI Assistant",
-    customStyles: {
-        botMessageBox: {
-            backgroundColor: "#D96EFF",
-        },
-        chatButton: {
-            backgroundColor: "#D96EFF",
-        },
-    },
+    customStyles: {},
     customComponents: {},
     state: {},
 }
